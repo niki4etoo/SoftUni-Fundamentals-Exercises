52cc1b4f4fb6a1a479a5ba6f7d80267179e2ea25
Nikolai Nanev <niki4etooo@gmail.com>
2024-03-13 09:35:20 +0200

    garage task 80/100

--- AssociativeArrays/more.js
+++ AssociativeArrays/more.js
@@ -13,8 +13,11 @@ function garage(input) {
 
         for (let g of garages) {
             if (Number(element[0]) == g.id) {
-                element = element.substring(3, element.length);
-                element = element.split(',');
+                element = element.substring(3, element.length).trim();
+
+                while(element.includes(":")){
+                    element = element.replace(": ", " - ");
+                }
 
                 g.info.push(element);
                 isAlreadyExist = true;
@@ -25,7 +28,11 @@ function garage(input) {
             garage.id = Number(element[0]);
             element = element.substring(3, element.length).trim();
 
-            garage.info = element;
+            while(element.includes(":")){
+                element = element.replace(": ", " - ");
+            }
+
+            garage.info = [element];
 
             garages.push(garage);
             garage = {};
@@ -33,8 +40,17 @@ function garage(input) {
         isAlreadyExist = false;
     }
 
-    console.log(garages);
+    for (let garage of garages) {
+        console.log(`Garage № ${garage.id}`);
+        for (let info of garage.info) {
+            console.log(`--- ${info}`);
+        }
+    }
 
 }
 
 garage(['1 - color: blue, fuel type: diesel', '1 - color: red, manufacture: Audi', '2 - fuel type: petrol', '4 - color: dark blue, fuel type: diesel, manufacture: Fiat']);
+garage(['1 - color: green, fuel type: petrol',
+    '1 - color: dark red, manufacture: WV',
+    '2 - fuel type: diesel',
+    '3 - color: dark blue, fuel type: petrol']);
